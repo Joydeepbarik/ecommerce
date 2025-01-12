de8c55e0d7589494a480e4060337e307cd5ee8a1
Joydeepbarik <joydeepbarik007@gmail.com>
2025-01-12 12:40:23 +0530

    backend name cgange

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -1,4 +1,4 @@
-export const BaseUrl = "http://localhost/Ecommerce-Backend";
+export const BaseUrl = "http://localhost/E-Commerce-Backend";
 export const Product_Image_URL = `${BaseUrl}/Product-images`;
 export const Blog_Image_URL = `${BaseUrl}/Blog-images`;
 
